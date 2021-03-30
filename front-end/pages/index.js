bf7450481af2c7b825fcebd3327fa77936f9c734
Varga Tamás <tamas.varga@smartitory.com>
2021-03-30 11:46:41 +0300

    changed js files, gitignore

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,9 +1,8 @@
 
 import Head from 'next/head'
-import TextInput from '../src/components/TextInput'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-import { HomeHeader } from '../src/components/HomeHeader'
+import { HomeHeader } from '../src/components/HomeHeader/HomeHeader'
 
 export default function Home() {
 
