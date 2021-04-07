7e4ff72575e04181419bb84c5bb83070a768c530
Greg <gergodemeny@gmail.com>
2021-04-07 12:19:39 +0300

    SQUAS-40 done

--- back-end/src/users/dto/update-user.dto.ts
+++ back-end/src/users/dto/update-user.dto.ts
@@ -1,12 +1,16 @@
 import ProfileDto from './profile.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class UpdateUserDto {
 	@ApiProperty({ required: false })
+	@IsEmail()
 	email: string;
+
 	@ApiProperty({ required: false })
-	role: string;
-	@ApiProperty({ required: false })
+	@ValidateNested({ each: true })
+	@Type(() => ProfileDto)
 	profile: ProfileDto;
 }
 
