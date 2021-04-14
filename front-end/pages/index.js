49bf6a8086e8cd282fccefc1d40f18ef6e0e8296
Maxim Gancean <maxim.gancean@smartitory.com>
2021-04-14 08:20:13 +0300

    fix

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,14 +1,11 @@
-
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import React from 'react'
-import { HomeHeader } from '../src/components/HomeHeader/HomeHeader'
+import React from 'react';
+import styles from '../styles/Home.module.css';
+import { HomeHeader } from '../src/components/HomeHeader/HomeHeader';
 
 export default function Home() {
-
-  return (
-    <div className={styles.container}>
-      <HomeHeader/>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<HomeHeader />
+		</div>
+	);
 }
