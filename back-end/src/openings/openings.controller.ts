d71b075032e60357b03690f811011b921bf10828
Greg <gergodemeny@gmail.com>
2021-04-19 01:21:28 +0300

    SQUAS-64, added permission guard

--- back-end/src/openings/openings.controller.ts
+++ back-end/src/openings/openings.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Put } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { OpeningsService } from './openings.service';
 import { UpdateOpeningDataDto } from './dto/update-opening-data.dto';
 import { OpeningDataDto } from './dto/opening-data.dto';
 import { ModificationResponseDto } from '../dto/modification.response.dto';
+import JwtAuthenticationGuard from '../auth/guards/jwt-authentication.guard';
+import { PermissionGuard } from '../admin/permission/guard/permission.guard';
+import { Target } from '../admin/permission/decorators/target.decorator';
+import { Operation } from '../admin/permission/decorators/permission.decorator';
 
 @Controller('openings')
 export class OpeningsController {
 	constructor(private openingsService: OpeningsService) {}
 
 	@Get()
+	@UseGuards(JwtAuthenticationGuard, PermissionGuard)
+	@Target('admin')
+	@Operation('read')
 	async index(): Promise<OpeningDataDto[]> {
 		return await this.openingsService.findAll();
 	}
 
 	@Put()
+	@UseGuards(JwtAuthenticationGuard, PermissionGuard)
+	@Target('admin')
+	@Operation('update')
 	async update(
 		@Body() dto: UpdateOpeningDataDto
 	): Promise<ModificationResponseDto> {
