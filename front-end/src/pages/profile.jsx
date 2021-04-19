3a87da3404c557c6a411eddd46d175f18f878d7e
Varga Tamás <tamas.varga@smartitory.com>
2021-04-19 15:57:32 +0300

    created profile page, some styling issue

--- front-end/src/pages/profile.jsx
+++ front-end/src/pages/profile.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import { HomeHeader } from '../components/HomeHeader'
+import React, { useState, useEffect } from "react";
+import { IconButton } from "@material-ui/core";
+import { Profile } from "views/Profile/Profile";
+import ProfileModal from "components/ProfileModal/ProfileModal";
+import EditIcon from "@material-ui/icons/Edit";
+import { makeStyles, Box } from "@material-ui/core";
 
 export default function profile() {
-    return (
-        <div>
-            <HomeHeader/>
-        </div>
-    )
-}
+  const [open, setOpen] = useState(false);
 
+  const useStyles = makeStyles(() => ({
+    container: {
+      display: "flex",
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    header: {
+      display: "flex",
+      flexDirection: "row",
+      backgroundColor: "red",
+    },
+    div: {
+      display: "flex",
+      width: "45%",
+      backgroundColor: "red",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.container}>
+      <Box className={classes.header}>
+        <Box className={classes.div}>
+          <p> Vezetéknév Keresztnév</p>
+          <p>Telefonszám</p>
+          <p>Email</p>
+        </Box>
+        <Box className={classes.div}>
+          <p>Kreditek száma</p>
+          <p>Következő foglalás</p>
+          <p>Kártyaszám</p>
+        </Box>
+
+        <Box className={classes.div}>
+          <IconButton onClick={() => setOpen(true)}>
+            <EditIcon />
+          </IconButton>
+
+          <ProfileModal open={open} onClose={() => setOpen(false)} />
+        </Box>
+      </Box>
+      <Profile />
+    </Box>
+  );
+}
