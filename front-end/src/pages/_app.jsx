5503c6a58600fbb6d321e80fe3b94b623a151740
Maxim Gancean <maxim.gancean@smartitory.com>
2021-05-03 11:20:12 +0300

    app

--- front-end/src/pages/_app.jsx
+++ front-end/src/pages/_app.jsx
@@ -29,30 +29,30 @@ const SmNext = (props) => {
 };
 
 SmNext.getInitialProps = async (appContext) => {
-	const { router, ctx } = appContext;
+	// const { router, ctx } = appContext;
 
-	const { store, req, res } = ctx;
+	// const { store, req, res } = ctx;
 
-	const { render, refreshToken } = ctx.query;
+	// const { render, refreshToken } = ctx.query;
 
-	// -- Cookie check --//
-	const { accessToken } = cookies(ctx);
+	// // -- Cookie check --//
+	// const { accessToken } = cookies(ctx);
 
-	if (accessToken) {
-		await store.dispatch(autoSignIn(cookies(ctx)));
+	// if (accessToken) {
+	// 	await store.dispatch(autoSignIn(cookies(ctx)));
 
-		if (req) {
-			try {
-				await Promise.all([store.dispatch(getMe())]);
-			} catch (error) {
-				res.clearCookie('accessToken');
-				res.clearCookie('refreshToken');
-				return res.redirect('/sign-in');
-			}
-		}
+	// 	if (req) {
+	// 		try {
+	// 			await Promise.all([store.dispatch(getMe())]);
+	// 		} catch (error) {
+	// 			res.clearCookie('accessToken');
+	// 			res.clearCookie('refreshToken');
+	// 			return res.redirect('/sign-in');
+	// 		}
+	// 	}
 
-		await store.dispatch(logged());
-	}
+	// 	await store.dispatch(logged());
+	// }
 
 	const appProps = await App.getInitialProps(appContext);
 	return { ...appProps };
