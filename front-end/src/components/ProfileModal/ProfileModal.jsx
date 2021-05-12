986905dbf035d0bcae048169d1e4f75af5baa456
Varga Tamás <tamas.varga@smartitory.com>
2021-05-12 16:08:31 +0300

    missing error, rewatch later

--- front-end/src/components/ProfileModal/ProfileModal.jsx
+++ front-end/src/components/ProfileModal/ProfileModal.jsx
@@ -1,5 +1,14 @@
 import React, { useRef } from 'react';
-import { Box,Button, Card, CardActions, CardContent, makeStyles, Modal, TextField } from '@material-ui/core';
+import {
+	Box,
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	makeStyles,
+	Modal,
+	TextField,
+} from '@material-ui/core';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { setProfile } from 'store/user/actions';
@@ -9,9 +18,7 @@ import useTranslation from 'next-translate/useTranslation';
 import { useErrorHandling } from 'components/error';
 import { validation } from './validation';
 
-
 const useStyles = makeStyles((theme) => ({
-	
 	modal: {
 		width: '75%',
 		height: '50%',
@@ -40,14 +47,14 @@ const useStyles = makeStyles((theme) => ({
 
 export const ProfileModal = (props) => {
 	const classes = useStyles();
-	const { user,open, onClose, defaultNamespace } = props
+	const { user, open, onClose, defaultNamespace } = props;
 
 	const { errorHandling, errorChecker } = useErrorHandling();
 	const { t } = useTranslation(defaultNamespace);
 
 	const dispatch = useDispatch();
 
-	const onSubmit = async (values,{setSubmitting}) => {
+	const onSubmit = async (values, { setSubmitting }) => {
 		console.log(values);
 		try {
 			await dispatch(
@@ -71,85 +78,93 @@ export const ProfileModal = (props) => {
 	};
 
 	return (
-		<Box >
-			{user && <Modal open={open} onClose={onClose}>
-				<Formik
-					validationSchema={validation}
-					validateOnChange
-					validateOnBlur
-					validateOnMount
-					onSubmit={onSubmit}
-					initialValues={{
-						firstName: user.profile.firstName,
-						lastName: user.profile.lastName,
-						email: user.email,
-						phone: user.profile.phone,
-					}}
-				>
-					{( formikProps) => {
-						const {handleChange,
-							values,
-							isSubmitting,
-							handleBlur} = formikProps;
-						return (
-							<Form noValidate>
-								<Card>
-									<CardContent className={classes.modal}>
-									<TextField
-										name='firstName'
-										value={values.firstName}
-										label={t('firstName')}
-										onChange={handleChange}
-										onBlur={handleBlur}
-										error={!!errorChecker(formikProps,'firstName')}
-										helperText={errorChecker(formikProps, 'firstName') || ' '}
-									
-									/>
-									<TextField
-										name='lastName'
-										value={values.lastName}
-										label={t('lastName')}
-										onChange={handleChange}
-										onBlur={handleBlur}
-										error={!!errorChecker(formikProps,'lastName')}
-										helperText={errorChecker(formikProps, 'lastName') || ' '}
-									/>
-									<TextField
-										value={values.email}
-										label={t('email')}
-										onChange={handleChange}
-										onBlur={handleBlur}
-										error={!!errorChecker(formikProps,'email')}
-										helperText={errorChecker(formikProps, 'email') || ' '}			
-										name='email'
-									/>
-									<TextField
-										name='phone'
-										value={values.phone}
-										label={t('phone')}
-										onChange={handleChange}
-										onBlur={handleBlur}
-										error={!!errorChecker(formikProps,'phone')}
-										helperText={errorChecker(formikProps, 'phone') || ' '}
-										
-									/>
-									</CardContent>
-									<CardActions className={classes.button} >
-										<Button 
-											color="primary"
-											disabled={isSubmitting}
-											type="submit"
-											variant="contained">
-											{t('save')}
-										</Button>
-									</CardActions>
-								</Card>
-							</Form>
-							)
-						}
-					}
-				</Formik>
-			</Modal>}
+		<Box>
+			{user && (
+				<Modal open={open} onClose={onClose}>
+					<Box>
+						<Formik
+							validationSchema={validation}
+							validateOnChange
+							validateOnBlur
+							validateOnMount
+							onSubmit={onSubmit}
+							initialValues={{
+								firstName: user.profile.firstName,
+								lastName: user.profile.lastName,
+								email: user.email,
+								phone: user.profile.phone,
+							}}
+						>
+							{(formikProps) => {
+								const {
+									handleChange,
+									values,
+									isSubmitting,
+									handleBlur,
+								} = formikProps;
+								return (
+									<Form noValidate>
+										<Card>
+											<CardContent className={classes.modal}>
+												<TextField
+													name="firstName"
+													value={values.firstName}
+													label={t('firstName')}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													error={!!errorChecker(formikProps, 'firstName')}
+													helperText={
+														errorChecker(formikProps, 'firstName') || ' '
+													}
+												/>
+												<TextField
+													name="lastName"
+													value={values.lastName}
+													label={t('lastName')}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													error={!!errorChecker(formikProps, 'lastName')}
+													helperText={
+														errorChecker(formikProps, 'lastName') || ' '
+													}
+												/>
+												<TextField
+													value={values.email}
+													label={t('email')}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													error={!!errorChecker(formikProps, 'email')}
+													helperText={errorChecker(formikProps, 'email') || ' '}
+													name="email"
+												/>
+												<TextField
+													name="phone"
+													value={values.phone}
+													label={t('phone')}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													error={!!errorChecker(formikProps, 'phone')}
+													helperText={errorChecker(formikProps, 'phone') || ' '}
+												/>
+											</CardContent>
+											<CardActions className={classes.button}>
+												<Button
+													color="primary"
+													disabled={isSubmitting}
+													type="submit"
+													variant="contained"
+												>
+													{t('save')}
+												</Button>
+											</CardActions>
+										</Card>
+									</Form>
+								);
+							}}
+						</Formik>
+					</Box>
+				</Modal>
+			)}
 		</Box>
 	);
 };
